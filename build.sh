7488107f93a7a421899252836e24985bc9bffbd5
suhail.ts <suhail.ts@terrficminds.com>
2025-09-22 14:28:21 +0530

    Add build script and update requirements for Python 3.11.9

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
